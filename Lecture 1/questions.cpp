bedeb916864003a1e0454d9f22cc4c00066adf53
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-02-16 12:01:57 +0530

    arrays and pointers

--- Lecture 1/questions.cpp
+++ Lecture 1/questions.cpp
@@ -54,14 +54,50 @@ int maxSubarraySum(int arr[], int n) {
 	return maxSum;
 }
 
+int maxSubarraySumOptimized(int arr[], int n) {
+	int maxSum = INT_MIN;
+
+	for (int i = 0; i < n; i++) {
+		int currSum = arr[i];
+
+		maxSum = max(currSum, maxSum);
+
+		for (int j = i + 1; j < n; j++) {
+
+			currSum += arr[j];
+
+			maxSum = max(currSum, maxSum);
+		}
+	}
+
+	return maxSum;
+}
+
+int kadanesAlgo(int arr[], int n) {
+
+	int maxSum = arr[0];
+	int currSum = arr[0];
+
+	for (int i = 1; i < n; i++) {
+
+		currSum = max(currSum + arr[i], arr[i]);
+
+		maxSum = max(currSum, maxSum);
+	}
+
+	return maxSum;
+}
+
 int main() {
 
-	int arr[] = {1, -2, 3, 4};
-	int n = 4;
+	int arr[] = {1, 3, 4, -2, 8, -98, 43, 2, -89, 6};
+	int n = 10;
 
-	printSubarrays(arr, n);
+	// printSubarrays(arr, n);
 
-	cout << maxSubarraySum(arr, n) << endl;
+	// cout << maxSubarraySum(arr, n) << endl;
+	// cout << maxSubarraySumOptimized(arr, n) << endl;
+	cout << kadanesAlgo(arr, n) << endl;
 
 	return 0;
 }
