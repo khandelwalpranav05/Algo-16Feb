b62f3af7a67cb02dcedef4e3fe21feebee2ecdcc
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-02-22 09:44:19 +0530

    recursion

--- Lecture 2/recursion_basics.cpp
+++ Lecture 2/recursion_basics.cpp
@@ -39,11 +39,88 @@ int factorial(int n) {
 	return total;
 }
 
+int power(int n, int p) {
+	if (p == 0) {
+		return 1;
+	}
+
+	int powen1 = power(n, p - 1);
+
+	int total = powen1 * n;
+
+	return total;
+}
+
+void printDecreasing(int n) {
+	if (n == 1) {
+		cout << n << endl;
+		return;
+	}
+
+	cout << n << endl;
+
+	printDecreasing(n - 1);
+}
+
+void printIncreasing(int n) {
+	if (n == 1) {
+		cout << n << endl;
+		return;
+	}
+
+	printIncreasing(n - 1);
+
+	cout << n << endl;
+}
+
+void printDecInc(int n) {
+	if (n == 0) {
+		return;
+	}
+
+	cout << n << endl;
+
+	printDecInc(n - 1);
+
+	cout << n << endl;
+}
+
+int linearSearch(int arr[], int si, int n, int data) {
+	if (si == n) {
+		return -1;
+	}
+
+	if (arr[si] == data) {
+		return si;
+	} else {
+		int rescursionResult = linearSearch(arr, si + 1, n, data);
+		return rescursionResult;
+	}
+}
+
+bool isSorted(int arr[], int si, int n) {
+
+}
+
 int main() {
 
 	// cout << sumTillN(5) << endl;
 
-	cout << factorial(5) << endl;
+	// cout << factorial(5) << endl;
+
+	// cout << power(2, 5) << endl;
+
+	// printDecreasing(5);
+
+	// printIncreasing(5);
+
+	// printDecInc(5);
+
+	int arr[] = {5, 3, 2, 6, 8, 9, 10};
+	int n = 7;
+	int data = 8;
+
+	cout << linearSearch(arr, 0, n, data) << endl;
 
 	return 0;
 }
