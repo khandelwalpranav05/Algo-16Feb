284b55d98f68e4533a8fafe86b4a6e7eb081bc15
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-03-16 23:03:11 +0530

    divide and conquer

--- Lecture 6/divideAndConquer.cpp
+++ Lecture 6/divideAndConquer.cpp
@@ -150,6 +150,179 @@ int painterProblem(int arr[], int n, int k) {
 	return ans;
 }
 
+bool canWePlace(int arr[], int n, int cows, long minDist) {
+
+	int abhiTakKiCows = 1;
+	int abhiTakKiPosition = arr[0];
+
+	for (int i = 1; i < n; i++) {
+
+		int dist = arr[i] - abhiTakKiPosition;
+
+		if (dist >= minDist) {
+			abhiTakKiCows++;
+			abhiTakKiPosition = arr[i];
+
+			if (abhiTakKiCows == cows) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+int aggressiveCows(int arr[], int n, int cows) {
+
+	sort(arr, arr + n);
+
+	long start = 0;
+	long end = arr[n - 1] - arr[0];
+
+	long ans;
+
+	while (start <= end) {
+
+		long mid = (start + end) / 2;
+
+		if (canWePlace(arr, n, cows, mid)) {
+			ans = mid;
+			start = mid + 1;
+		} else {
+			end = mid - 1;
+		}
+	}
+
+	return ans;
+}
+// s                    e
+// {2, 4, 6, 7, 1, 3, 5, 9}
+
+void merge(int arr[], int start, int end) {
+
+	int mid = (start + end) / 2;
+
+	int i = start;
+	int j = mid + 1;
+
+	int temp[end - start + 1];
+	int k = 0;
+
+	while (i <= mid and j <= end) {
+
+		if (arr[i] <= arr[j]) {
+			temp[k] = arr[i];
+			i++;
+			k++;
+		} else {
+			temp[k] = arr[j];
+			j++;
+			k++;
+		}
+	}
+
+	while (j <= end) {
+		temp[k] = arr[j];
+		j++;
+		k++;
+	}
+
+	while (i <= mid) {
+		temp[k] = arr[i];
+		i++;
+		k++;
+	}
+
+	int p = 0;
+	for (int x = start; x <= end; x++) {
+		arr[x] = temp[p];
+		p++;
+	}
+
+}
+
+void mergeSort(int arr[], int start, int end) {
+	if (start == end) {
+		return;
+	}
+
+	int mid = (start + end) / 2;
+
+	mergeSort(arr, start, mid);
+	mergeSort(arr, mid + 1, end);
+
+	merge(arr, start, end);
+}
+
+int find(vector<int> &nums, int start, int end) {
+
+	int n = nums.size();
+
+	int mid = (start + end) / 2;
+
+	if ((mid == 0 or nums[mid] > nums[mid - 1]) and (mid == n - 1 or nums[mid] > nums[mid + 1])) {
+		return mid;
+	}
+
+	if (nums[mid] > nums[mid + 1]) {
+		return find(nums, start, mid - 1);
+	} else {
+		return find(nums, mid + 1, end);
+	}
+}
+
+bool isValid(vector<int> &nums, int m, long maxSum) {
+
+	int split = 1;
+	long splitSum = 0;
+
+	for (int i = 0; i < nums.size(); i++) {
+
+		splitSum += nums[i];
+
+		if (splitSum > maxSum) {
+			split++;
+			splitSum = nums[i];
+
+			if (split > m) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+int splitArray(vector<int>& nums, int m) {
+
+	int minSum = nums[0];
+	long maxSum = nums[0];
+
+	for (int i = 1; i < nums.size(); i++) {
+		minSum = max(minSum, nums[i]);
+		maxSum += nums[i];
+	}
+
+	long start = minSum;
+	long end = maxSum;
+
+	long ans;
+
+	while (start <= end) {
+
+		long mid = (start + end) / 2;
+
+		if (isValid(nums, m, mid)) {
+			ans = mid;
+			end = mid - 1;
+		} else {
+			start = mid + 1;
+		}
+	}
+
+	return ans;
+}
+
 int main() {
 
 	// int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -170,11 +343,27 @@ int main() {
 
 	// cout << searchRotatedArray(arr, 0, n - 1, data) << endl;
 
-	int board[] = {10, 20, 30, 40};
-	int n = 4;
-	int k = 2;
+	// int board[] = {10, 20, 30, 40};
+	// int n = 4;
+	// int k = 2;
 
-	cout << painterProblem(board, n, k) << endl;
+	// cout << painterProblem(board, n, k) << endl;
+
+	// int arr[] = {1 , 2 , 8 , 4 , 9};
+	// int n = 5;
+	// int cows = 3;
+
+	// cout << aggressiveCows(arr, n, cows) << endl;
+
+	// int arr[] = {8, 7, 6, 5, 4, 3, 2, 1};
+	// int n = 8;
+
+	// mergeSort(arr, 0, n - 1);
+
+	// for (int i = 0; i < n; i++) {
+	// 	cout << arr[i] << " ";
+	// }
+	// cout << endl;
 
 	return 0;
 }
