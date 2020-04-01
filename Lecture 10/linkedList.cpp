4dc9b96b585b92db3725a9d66672eca51c435101
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-01 20:26:00 +0530

    linkedList

--- Lecture 10/linkedList.cpp
+++ Lecture 10/linkedList.cpp
@@ -392,6 +392,24 @@ int main() {
 
 	// display(head);
 
+	// node* head = NULL;
+
+	// insertAtTail(head, 1);
+	// insertAtTail(head, 2);
+	// insertAtTail(head, 3);
+	// insertAtTail(head, 4);
+	// insertAtTail(head, 5);
+	// insertAtTail(head, 6);
+	// insertAtTail(head, 7);
+	// insertAtTail(head, 8);
+	// insertAtTail(head, 9);
+
+	// display(head);
+
+	// head = kReverse(head, 3);
+
+	// display(head);
+
 	node* head = NULL;
 
 	insertAtTail(head, 1);
@@ -399,16 +417,10 @@ int main() {
 	insertAtTail(head, 3);
 	insertAtTail(head, 4);
 	insertAtTail(head, 5);
-	insertAtTail(head, 6);
-	insertAtTail(head, 7);
-	insertAtTail(head, 8);
-	insertAtTail(head, 9);
 
 	display(head);
 
-	head = kReverse(head, 3);
-
-	display(head);
+	printInReverse(head); // 5-4-3-2-1
 
 	return 0;
 }
