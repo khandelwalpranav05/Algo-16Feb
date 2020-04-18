b2a393b05f44a0d026dd0fd79930f0edc53873b4
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-18 22:49:41 +0530

    trees

--- Lecture 15/bst.cpp
+++ Lecture 15/bst.cpp
@@ -17,6 +17,25 @@ public:
 	}
 };
 
+TreeNode* builtTree(TreeNode* root) {
+
+	int data;
+	cin >> data;
+
+	if (data == -1) {
+		return NULL;
+	}
+
+	if (root == NULL) {
+		root = new TreeNode(data);
+	}
+
+	root->left = builtTree(root->left);
+	root->right = builtTree(root->right);
+
+	return root;
+}
+
 TreeNode* builtBST(TreeNode* root, int data) {
 	if (root == NULL) {
 		return new TreeNode(data);
@@ -67,21 +86,67 @@ void inOrder(TreeNode* root) {
 	inOrder(root->right);
 }
 
+
+// Time: O()
 bool search(TreeNode* root, int key) {
-	//TODO
+	if (root == NULL) return false;
+
+	if (root->val == key) return true;
+
+	if (key > root->val) {
+		return search(root->right, key);
+	} else {
+		return search(root->left, key);
+	}
 }
+
+bool isBST(TreeNode* root, int minRange = INT_MIN, int maxRange = INT_MAX) {
+	if (root == NULL) {
+		return true;
+	}
+
+	bool leftBST = isBST(root->left, minRange, root->val);
+	bool rightBST = isBST(root->right, root->val, maxRange);
+
+	if (leftBST and rightBST and root->val > minRange and root->val < maxRange) {
+		return true;
+	}
+
+	return false;
+}
+
+class TreeDetail {
+public:
+	int size;
+	bool bst;
+	int minValue;
+	int maxValue;
+};
+
+// TreeDetail largestBstInBinaryTree(TreeNode* root) {}
 
 int main() {
 
 	TreeNode* root = insert();
 
-	preOrder(root);
-	cout << endl;
+	// TreeNode* root = NULL;
+	// root = builtTree(root);
 
-	inOrder(root);
-	cout << endl;
+	// preOrder(root);
+	// cout << endl;
+
+	// inOrder(root);
+	// cout << endl;
+
+	// cout << search(root, 5) << endl;
+
+	// cout << isBST(root) << endl;
+
 
 	return 0;
 }
 
 // 4 2 1 3 6 5 7 -1
+
+//leftview
+// 1 2 4 -1 6 -1 -1 -1 3 -1 5 7 9 -1 -1 10 11 -1 -1 -1 8 -1 -1
