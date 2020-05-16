f5e82536f17ff151874b7201234f4b3ba1cfe456
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-05-16 18:05:47 +0530

    dp

--- Dynamic Programming/dp_basics.cpp
+++ Dynamic Programming/dp_basics.cpp
@@ -688,6 +688,29 @@ int minPathSum(vector<vector<int>>& grid) {
 	return helper(grid, 0, 0, dp);
 }
 
+int maxEnvelopes(vector<vector<int> > &envelopes) {
+
+	int n = envelopes.size();
+
+	if (n == 0) {
+		return 0;
+	}
+
+	sort(envelopes.begin(), envelopes.end());
+
+	vector<int> dp(n, 1);
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (envelopes[i][0] > envelopes[j][0] and envelopes[i][1] > envelopes[j][1]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+
+	return *max_element(dp.begin(), dp.end());
+}
+
 int main() {
 
 	// int n = 5;
